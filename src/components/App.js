3339e3302866cb169a9f36c4e7909c84acd4c472
sergey <sershram66@gmail.com>
2022-06-22 23:17:14 +0300

    Refactoring of NavBar component and create NotFound

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import './App.css';
 import Header from "./Common/Header/Header";
 import Footer from "./Common/Footer/Footer";
 import Home from "./Home/Home";
 import {Route} from "react-router-dom";
 import Profile from "./Profile/Profile";
 import ContactsContainer from "./Contacts/ContactsContainer";
+import NotFound from "./Common/NotFound/NotFound";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Header/>
-            <Route exact path='/' component={Home}/>
-            <Route path='/profile' component={Profile} />
-            <Route path='/contacts' component={ContactsContainer} />
-            <Route path='/contacts/:id' component={ContactsContainer} />
-        <Footer/>
+      <Header/>
+      <Route exact path='/' component={Home}/>
+      <Route path='/profile' component={Profile}/>
+      <Route path='/contacts' component={ContactsContainer}/>
+      <Route path='/contacts/:id' component={ContactsContainer}/>
+      {/*<Route path='*' component={NotFound}/>*/}
+      <Footer/>
     </div>
   );
 }
--- src/components/Common/NavBar/NavBar.js
+++ src/components/Common/NavBar/NavBar.js
@@ -1,28 +1,40 @@
-import './NavBar.scss'
 import {NavLink} from "react-router-dom";
-import React from "react";
+import './NavBar.scss';
+
+const jsonRoutes = {
+  "routes": [
+    {
+      "id": "1",
+      "route": "/",
+      "component": "Home"
+    },
+    {
+      "id": "2",
+      "route": "/contacts",
+      "component": "Contacts"
+    }
+  ]
+}
 
 function NavBar() {
-    return (
-        <nav>
-            <ul className='nav_list'>
-                <li className="nav_item">
-                    <NavLink
-                        to="/"
-                        activeClassName="active">
-                        Home
-                    </NavLink>
-                </li>
-                <li className="nav_item">
-                    <NavLink
-                        to="Contacts"
-                        activeClassName="active">
-                        Contacts
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav>
+      <ul className='nav_list'>
+        {
+          jsonRoutes.routes.map(item =>
+            <li className="nav_item" key={item.id}>
+              <NavLink
+                exact
+                to={item.route}
+                activeClassName="active"
+              >
+                {item.component}
+              </NavLink>
+            </li>)
+        }
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
